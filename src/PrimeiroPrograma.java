1882d010cd77b73151a076fa4eef9d0117131c22
Lucas Souza <lucas23500@gmail.com>
2022-09-30 00:20:59 -0400

    First Commit

---
+++ src/PrimeiroPrograma.java
@@ -0,0 +1,43 @@
+package src;
+
+public class PrimeiroPrograma {
+    public static void main(String[] args) {
+
+        Livro livro = new Livro("Jogador Nº 1", 351);
+        System.out.println(livro);
+    }
+}
+
+class Livro{
+    private String nome;
+    private Integer numPaginas;
+
+    Livro (String nome, Integer numPaginas){
+        this.nome = nome;
+        this.numPaginas = numPaginas;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getNumPaginas() {
+        return numPaginas;
+    }
+
+    public void setNumPaginas(Integer numPaginas) {
+        this.numPaginas = numPaginas;
+    }
+
+    @Override
+    public String toString() {
+        return "Livro{" +
+                "nome='" + nome + '\'' +
+                ", numPaginas=" + numPaginas +
+                '}';
+    }
+}
